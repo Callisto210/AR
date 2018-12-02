14f0297ae0bf53a4b0ad4c016b8f512961ea8284
Callisto210 <mikro16@o2.pl>
2018-12-03 00:04:33 +0100

    Lab4 added

---
+++ lab4/main.c
@@ -0,0 +1,130 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <mpi.h>
+
+int cmpfunc(const void * a, const void * b) {
+	return ( *(int*)a - *(int*)b );
+}
+
+
+void merge_lesser(int *result, int *a, int *b, size_t size) {
+	size_t i, j=0, k=0;
+
+	for (i=0; i < size; i++) {
+		if (a[j] < b[k])
+			result[i] = a[j++];
+		else
+			result[i] = b[k++];
+	}
+}
+
+void merge_greater(int *result, int *a, int *b, size_t size) {
+	int i, j=size-1, k=size-1;
+
+	for (i=size-1; i >= 0; i--) {
+		if (a[j] > b[k])
+			result[i] = a[j--];
+		else
+			result[i] = b[k--];
+	}
+}
+
+int two_to_power[] = {1, 2, 4, 8};
+int cpus_to_dim[] = {0, 0, 1, 2, 2, 3, 3, 3, 3};
+
+int main(int argc, char** argv) {
+	int world_rank, partner;
+	int world_size;	
+	int n;
+	int i, m;
+	int array_size;
+	int *array = NULL;
+	int sub_array_size;
+	int *sub_array, *foreign_sub_array, *result_array, *tmp;
+
+	MPI_Init(&argc, &argv);
+	MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+	
+	if (world_rank == 0) {
+		n = atoi(argv[1]);
+		array = malloc(n * sizeof(int));
+		srand(time(NULL));
+		for(i = 0; i < n; i++) {
+			array[i] = rand() % n;
+		}
+	}
+
+	/* Broadcast number of elements to others */
+	MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
+	//printf("Number of all elements %d\n", n);
+
+	sub_array_size = n/world_size;
+
+	sub_array = malloc(sub_array_size * sizeof(int));
+	foreign_sub_array = malloc(sub_array_size * sizeof(int));
+	result_array = malloc(sub_array_size * sizeof(int));
+
+	/* Send part of array to processes */
+	MPI_Scatter(array, sub_array_size, MPI_INT, sub_array, sub_array_size, MPI_INT, 0, MPI_COMM_WORLD);
+
+	if (world_rank == 0) {
+		free(array);
+	}
+
+#if 0
+	for (int i = 0; i < world_size; i++) {
+		MPI_Barrier(MPI_COMM_WORLD);
+		if (i == world_rank) {
+			printf("CPU %d: ", world_rank);
+			for (m = 0; m < sub_array_size; m++) {
+				printf("%d ", sub_array[m]);
+				fflush(stdout);
+			}
+			printf("\n");
+		}
+	}
+#endif
+	qsort(sub_array, sub_array_size, sizeof(*sub_array), cmpfunc);
+
+	/* Perform merge */
+	for (i=1; i<=cpus_to_dim[world_size]; i++) {
+		MPI_Barrier(MPI_COMM_WORLD);
+		for (m=i-1; m>=0; m--) {
+			partner = world_rank ^ two_to_power[m];
+			if (partner < world_size) {
+				/* Perform compare & exchange */
+				MPI_Sendrecv(	sub_array, sub_array_size, MPI_INT, partner, 0,
+						foreign_sub_array, sub_array_size, MPI_INT, partner, 0,
+						MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+				printf("CPU %d to %d\n", world_rank, partner);
+				if (world_rank && two_to_power[i])
+					merge_lesser(result_array, sub_array, foreign_sub_array, sub_array_size);
+				else
+					merge_greater(result_array, sub_array, foreign_sub_array, sub_array_size);
+
+				tmp = sub_array;
+				sub_array = result_array;
+				result_array = tmp;
+			}
+		}
+	}
+
+
+	for (int i = 0; i < world_size; i++) {
+		MPI_Barrier(MPI_COMM_WORLD);
+		if (i == world_rank) {
+			printf("CPU %d: ", world_rank);
+			for (m = 0; m < sub_array_size; m++) {
+				printf("%d ", sub_array[m]);
+				fflush(stdout);
+			}
+			printf("\n");
+		}
+	}
+
+	MPI_Finalize();
+	return (0);
+
+}
---
+++ lab4/skrypt.sh
@@ -0,0 +1,16 @@
+for size in 3000 5000 7000 9000 11000 13000 15000 17000 19000 21000
+do
+	python generator.py $size > input.txt
+#	cat input.txt | ./seq >> results.txt
+	cat input.txt | ./seq2 >> results2.txt
+
+#	for cpus in {2..8}
+#	do
+#		mpiexec -np $cpus ./first $size >> results.txt
+#	done
+
+#	for cpus in {2..8}
+#	do
+#		mpiexec -np $cpus ./second $size >> results.txt
+#	done
+done
